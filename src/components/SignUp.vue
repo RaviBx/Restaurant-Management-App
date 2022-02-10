08de4a3e02dc591e7a9514209cc5b29175ee692a
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-11 00:05:53 +0530

    Added SignUp Component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    
+    <img class="logo" src="../assets/restro.png" />
+    <h1>Hello</h1>
+
+    <div>
+            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Enter Name" />
+
+    </div>
+
+</div>
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+
+<style>
+
+    .logo{
+        width: 150px;
+    }
+
+</style>
