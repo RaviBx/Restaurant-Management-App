82bcd399a460a271348ef189292bba3d543391f5
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-11 14:51:09 +0530

    Added axios package

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -5,11 +5,11 @@
     <h1>Sign Up</h1>
 
     <div class="register">
-            <input type="text" placeholder="Enter Name" />
-            <input type="text" placeholder="Enter Email" />
-            <input type="password" placeholder="Enter Password" />
+            <input type="text" v-model="name" placeholder="Enter Name" />
+            <input type="text" v-model="email" placeholder="Enter Email" />
+            <input type="password" v-model="password" placeholder="Enter Password" />
 
-            <button>Sign Up</button>
+            <button v-on:click="signUp">Sign Up</button>
 
     </div>
 
@@ -21,9 +21,26 @@
 
 <script>
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data()
+    {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods:{
+        signUp()
+        {
+            console.warn("Signup")
+        }
+    }
 }
 </script>
+
+
+
 
 
 <style>
